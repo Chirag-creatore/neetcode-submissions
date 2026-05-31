5b12e4db74578f5cf154eb44ae2adc8031eedf79
Chirag Singh <chirag9090.singh9090@gmail.com>
2026-05-31 09:32:03 +0530

    Add: number-of-subsequences-that-satisfy-the-given-sum-condition - submission-1

---
+++ Data Structures & Algorithms/number-of-subsequences-that-satisfy-the-given-sum-condition/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    private final int M = 1000000007;
+
+    private int[] pow = new int[100000];
+    Solution(){
+        pow[0] = 1;
+        for(int i = 1; i < 100000; i++){
+            pow[i] = pow[i - 1] * 2 % M;
+        }
+    }
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int j = 0;
+        int n = nums.length;
+        while(j < n && nums[0] + nums[j] <= target){
+            j++;
+        }
+        j--;
+
+        int ans = 0;
+        int i = 0;
+        while(i <= j){
+            if(nums[i] + nums[j] <= target){
+                ans += pow[j - i];
+                ans %= M;
+                i++;
+            }else{
+                j--;
+            }
+        }
+
+        return ans;
+    }
+}
